ab86fb0fa392df0eb8dbe3ab07a3717d7e34c2fa
xaviguasch <xaviguasch@gmail.com>
2023-03-04 15:49:59 +0100

    added j02-reverse-an-array challenge

---
+++ j02-reverse-an-array/reverse-an-array.js
@@ -0,0 +1,14 @@
+// Reverse an array
+// You have been asked to write a function called reverseAnArray(arr) that takes in one array arr as the argument. The function should reverse the order of the elements in the array and return the reversed array.
+
+// Directions
+// If the input array is empty, then return an empty array.
+// The input array will only contain strings and numbers
+
+const reverseAnArray = (arr) => {
+  return
+}
+
+console.log(reverseAnArray([1, 2, 3, 4, 5]))
+console.log(reverseAnArray(['e', 'd', 'c', 'b', 'a']))
+console.log(reverseAnArray([1, 2]))
