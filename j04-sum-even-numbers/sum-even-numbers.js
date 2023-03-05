10ebe8ce2b7c69df268bd6e8ae0c5db5a50d37ea
xaviguasch <xaviguasch@gmail.com>
2023-03-05 16:24:06 +0100

    added alternative solution to previous challenge

--- j04-sum-even-numbers/sum-even-numbers.js
+++ j04-sum-even-numbers/sum-even-numbers.js
@@ -15,8 +15,17 @@ const sumOfEvens = (arr) => {
       return acc
     }
   }, 0)
+
+  // ALTERNATIVE SOLUTION USING LOOPS
+  // let sum = 0
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] % 2 === 0) {
+  //     sum += arr[i]
+  //   }
+  // }
+  // return sum
 }
 
-// console.log(sumOfEvens([1, 1, 1, 2, 2, 2, 3, 3, 3])) // 6
-// console.log(sumOfEvens([])) // 0
+console.log(sumOfEvens([1, 1, 1, 2, 2, 2, 3, 3, 3])) // 6
+console.log(sumOfEvens([])) // 0
 console.log(sumOfEvens([-5, 0, 5, -4, 1, 6, -3, 2, 7])) // 4
