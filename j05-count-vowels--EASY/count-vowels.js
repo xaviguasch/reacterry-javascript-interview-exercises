728c242b2b0f51a392814f4442361dc068e02971
xaviguasch <xaviguasch@gmail.com>
2023-03-07 09:31:15 +0100

    added j05-count-vowels EASY challenge

---
+++ j05-count-vowels--EASY/count-vowels.js
@@ -0,0 +1,14 @@
+// Count vowels
+// Write a function countVowels(str), which takes a string as an input. This function should return the total number of vowels in the string. Return 0 if no vowels are found.
+
+// Directions
+// Return 0 if the input has no vowels
+// The string can consist of any alphanumeric characters
+
+const countVowels = (str) => {
+  return
+}
+
+console.log(countVowels('Hello world!')) // 3
+console.log(countVowels('This is a test')) // 4
+console.log(countVowels('No vowels here')) // 0
