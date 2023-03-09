ab656cd6cc6d36a890c3326fe826fb8e6829ee9e
xaviguasch <xaviguasch@gmail.com>
2023-03-09 20:49:34 +0100

    added solution to previous challenge

--- j08-sum-every-third-number--EASY/sum-every-third-number.js
+++ j08-sum-every-third-number--EASY/sum-every-third-number.js
@@ -6,9 +6,16 @@
 // The input array will contain only numbers
 
 const sumOfThirds = (arr) => {
-  return
+  let sum = 0
+
+  for (let i = 0; i < arr.length; i += 3) {
+    sum += arr[i]
+  }
+
+  return sum
 }
 
 console.log(sumOfThirds([1, 1, 1, 2, 2, 2, 3, 3, 3])) // 6
 console.log(sumOfThirds([])) // 0
 console.log(sumOfThirds([-5, 0, 5, -4, 1, 6, -3, 2, 7])) // -12
+console.log(sumOfThirds([1, 2, 3, 4, 5, 6])) // 5
