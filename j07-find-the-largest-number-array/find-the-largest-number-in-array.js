e97b8f7748bf8274a8abccd6333adfe1a1791443
xaviguasch <xaviguasch@gmail.com>
2023-03-09 20:19:03 +0100

    added j07-find-the-largest-number-in-array EASY challenge

---
+++ j07-find-the-largest-number-array/find-the-largest-number-in-array.js
@@ -0,0 +1,14 @@
+// Find the largest number in array
+// Write a function findMax(arr), which takes an array of numbers as the input. This function should return the biggest number in the array. The highest number may occur more than once. If no such number is found then return null.
+
+// Directions
+// The input array will contain only numbers
+// Return null if no max number is found
+
+const findMax = (arr) => {
+  return
+}
+
+console.log(findMax([4, 7, 2, 9, 1, 5])) // 9
+console.log(findMax([-1, -5, -2])) // -1
+console.log(findMax([])) // null
