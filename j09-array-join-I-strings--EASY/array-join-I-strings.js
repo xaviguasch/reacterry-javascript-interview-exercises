3bd473843b1b1a0ec8805d3a032745de77d48be1
xaviguasch <xaviguasch@gmail.com>
2023-03-09 20:58:34 +0100

    added j09-array-join-i-strings EASY challenge

---
+++ j09-array-join-I-strings--EASY/array-join-I-strings.js
@@ -0,0 +1,20 @@
+// Array.join I - strings
+// In this coding challenge, you will be implementing a function called joinElementsWithUnderscore that takes an array as input and returns a string. The returned string should consist of all the array elements joined by an underscore _. If the input array is empty, the function should return an empty string.
+
+// Your implementation should be efficient and avoid unnecessary looping and variable creation.
+
+// Directions
+// The joinElementsWithUnderscore function should take one argument: the input array.
+// The returned string should consist of all the elements of the input array joined by an underscore _. The underscore should be placed between adjacent elements in the output string.
+// Return an empty string if the input array is empty
+
+const joinElementsWithUnderscore = (array) => {
+  // Write your code here
+
+  return
+}
+
+const array = ['apple', 'banana', 'orange', 'grape']
+const joinedString = joinElementsWithUnderscore(array)
+console.log(joinedString)
+// Output: 'apple_banana_orange_grape'
