5006d07e3f9da2ecff1dc4ad23d77df6d9b1765d
xaviguasch <xaviguasch@gmail.com>
2023-03-09 20:28:50 +0100

    added j08-sum-every-third-number EASY challenge

---
+++ j08-sum-every-third-number--EASY/sum-every-third-number.js
@@ -0,0 +1,14 @@
+// Sum every third number
+// Write a function sumOfThirds(arr), which takes an array as an argument. This function should return a sum of every third number in the array, starting from the first one.
+
+// Directions
+// If the input array is empty then return 0
+// The input array will contain only numbers
+
+const sumOfThirds = (arr) => {
+  return
+}
+
+console.log(sumOfThirds([1, 1, 1, 2, 2, 2, 3, 3, 3])) // 6
+console.log(sumOfThirds([])) // 0
+console.log(sumOfThirds([-5, 0, 5, -4, 1, 6, -3, 2, 7])) // -12
