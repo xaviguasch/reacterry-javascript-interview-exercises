acb7d70850f0ad265bb5daac0270d4192857fba7
xaviguasch <xaviguasch@gmail.com>
2023-03-10 20:44:27 +0100

    added j10-reverse-a-string EASY challenge

---
+++ j10-reverse-a-string--EASY/reverse-a-string.js
@@ -0,0 +1,13 @@
+// Reverse a string
+// You have been asked to write a function reverseAString(str) that takes in a string as an argument. The function should return a new string that is the reverse of the input string. The function should not modify the original string.
+
+// Directions
+// Strings are immutable, so return a new string
+// If the input string is empty, then return an empty string
+
+const reverseAString = (str) => {
+  return
+}
+
+console.log(reverseAString('hello')) // 'olleh'
+console.log(reverseAString('hi')) // 'ih'
