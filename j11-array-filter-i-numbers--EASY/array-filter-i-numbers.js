6d0af2c801468b7c8e2095bc7ef7cbf469b8dffb
xaviguasch <xaviguasch@gmail.com>
2023-03-14 22:05:41 +0100

    added j11-array-filter-i-numbers EASY challenge

---
+++ j11-array-filter-i-numbers--EASY/array-filter-i-numbers.js
@@ -0,0 +1,11 @@
+// Array.filter I - numbers
+// Implement a function called getLessThanFive that takes an array of numbers as input and returns a new array containing only the elements that are strictly less than 5.
+
+// Directions
+// If no elements in the input array are less than 5, return an empty array.
+
+const getLessThanFive = (numbers) => {
+  return
+}
+
+console.log(getLessThanFive([2, 6, 3, 7, 1, 9, 4]))
