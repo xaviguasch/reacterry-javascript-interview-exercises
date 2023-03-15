0ff660c73634b6b36a2037b15d83af89ac482b92
xaviguasch <xaviguasch@gmail.com>
2023-03-15 20:36:35 +0100

    added j13-array-map-i-strings EASY challenge

---
+++ j13-array-map-i-strings--EASY/array-map-i-strings.js
@@ -0,0 +1,14 @@
+// Array.map I - strings
+// Write a function called mapToUppercase(names), which takes an array of strings called names as its argument. This function should return a new array with all of the strings transformed to UPPERCASE.
+
+// Directions
+// The input array names will contain only strings.
+// Do not modify the existing array. Return a new array with transformed strings.
+// If the input array names is empty, then return an empty array.
+
+const mapToUppercase = (names) => {
+  return
+}
+
+const names1 = ['john', 'mary', 'bob', 'jane']
+console.log(mapToUppercase(names1))
