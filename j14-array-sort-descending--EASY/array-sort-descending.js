01aad7973e94f1e78a0b053cf53d450520710e24
xaviguasch <xaviguasch@gmail.com>
2023-03-16 22:11:51 +0100

    added j14-array-sort-descending EASY challenge

---
+++ j14-array-sort-descending--EASY/array-sort-descending.js
@@ -0,0 +1,14 @@
+// Array.sort - descending
+// In this coding challenge, you will be implementing a function called sortDescending, which takes in an array of numbers as input and returns the same array but sorted in descending order.
+
+// Your implementation should sort the input array in place, without creating a new array. If the input array is empty, your implementation should return an empty array.
+
+// Directions
+// You should not create a new array in your implementation.
+// Your implementation should be efficient and avoid unnecessary looping and variable creation.
+
+const sortDescending = (numbers) => {
+  return
+}
+
+console.log(sortDescending([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]))
