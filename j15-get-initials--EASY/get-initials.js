48354a2f423520d84cd562b53a8b7b882c1636b6
xaviguasch <xaviguasch@gmail.com>
2023-03-22 16:53:14 +0100

    added j15-get-initials EASY challenge

---
+++ j15-get-initials--EASY/get-initials.js
@@ -0,0 +1,13 @@
+// Get initials
+// Write a function getInitials(user), which takes an object user as an argument. The user object will always have at least two properties firstName and lastName, which hold string values. Some objects may also have the additional property middleName. The function should return the user's initials in the following format: F. M. L., where the letters F, M and L represent the first, middle and last names respectively.
+
+// Directions
+// Remember to follow the required return format
+// You can assume that the input object will always have firstName and lastName
+
+const getInitials = (user) => {
+  return
+}
+
+console.log(sumNumbers({ firstName: 'John', middleName: 'A', lastName: 'Doe' }))
+console.log(sumNumbers({ firstName: 'Jane', lastName: 'Doe' }))
