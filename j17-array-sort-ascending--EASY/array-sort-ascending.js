1fa1b826b456ef16ea9137dcc2c611ec5e129ff2
Xavi Guasch <xaviguasch@gmail.com>
2023-03-27 19:24:58 +0200

    added j17-array-sort-ascending EASY challenge

---
+++ j17-array-sort-ascending--EASY/array-sort-ascending.js
@@ -0,0 +1,14 @@
+// Array.sort - ascending
+// In this coding challenge, you will be implementing a function called sortAscending(numbers), which takes in an array of numbers as input and returns the same array but sorted in ascending order.
+
+// Directions
+// If the input array is empty, return an empty array.
+// Sort the array in place.
+
+const sortAscending = (numbers) => {
+  return
+}
+
+const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+sortAscending(numbers)
+console.log(numbers)
