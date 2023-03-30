7168e5ecd8e3c2d00efe857338367b5bc36f09e9
Xavi Guasch <xaviguasch@gmail.com>
2023-03-30 19:54:26 +0200

    added j20-object-values EASY challenge

---
+++ j20-object-values--EASY/object-values.js
@@ -0,0 +1,13 @@
+// Object.values
+// In this coding challenge, you will be implementing a function called getValues(object) that takes an argument: an object. The function should return an array will all of the object’s values.
+
+// Directions
+// If the object has no values, return an empty array.
+
+const getValues = (object) => {
+  return
+}
+
+console.log(getValues({ foo: 1, bar: 2, baz: 3 }))
+console.log(getValues({ foo: 1, bar: 2 }))
+console.log(getValues({}))
