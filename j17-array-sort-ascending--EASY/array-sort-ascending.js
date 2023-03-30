9d51103f7c7fce0f812c04020ea929c468d999a6
Xavi Guasch <xaviguasch@gmail.com>
2023-03-30 19:45:54 +0200

    added j18-array-pop EASY challenge

--- j17-array-sort-ascending--EASY/array-sort-ascending.js
+++ j17-array-sort-ascending--EASY/array-sort-ascending.js
@@ -6,7 +6,7 @@
 // Sort the array in place.
 
 const sortAscending = (numbers) => {
-  return
+  return numbers.sort((a, b) => a - b)
 }
 
 const numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
---
+++ j18-array-pop--EASY/array-pop.js
@@ -0,0 +1,13 @@
+// Array.pop
+// In this coding challenge, you will be implementing a function called remove(array) that takes one argument: an array. The function should remove the last element from the input array and return the array.
+
+// Directions
+
+const remove = (array) => {
+  // Write your code here
+
+  return
+}
+
+console.log(remove([1, 2, 3]))
+console.log(remove(['foo', 'bar', 'baz']))
