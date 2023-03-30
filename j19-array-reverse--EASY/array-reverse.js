0a5fc4e1dd4f6f083c1b3f63da7bf10f2eb2694f
Xavi Guasch <xaviguasch@gmail.com>
2023-03-30 19:49:40 +0200

    added j19-array-reverse EASY challenge

---
+++ j19-array-reverse--EASY/array-reverse.js
@@ -0,0 +1,13 @@
+// Array.reverse
+// In this coding challenge, you will be implementing a function called reverseArray(array, value) that takes one argument: an array. The function should reverse the order of the elements in the array.
+
+// Directions
+// If the input array is empty, return an empty array.
+
+const reverseArray = (array) => {
+  return
+}
+
+console.log(reverseArray([1, 2, 3, 4]))
+console.log(reverseArray([1, 2]))
+console.log(reverseArray([]))
