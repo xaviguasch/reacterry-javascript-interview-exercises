ac005c56e5c34e29f1690db10e8f204eef617767
Xavi Guasch <xaviguasch@gmail.com>
2023-03-31 19:12:17 +0200

    added j23-string-indexOf EASY challenge

---
+++ j23-string-indexOf--EASY/string-indexof.js
@@ -0,0 +1,13 @@
+// String.indexOf
+// In this coding challenge, you will be implementing a function called findIndexOf(string, substring) that takes two arguments: a string and a substring. The function should return the index of the first occurrence of the value in the string.
+
+// Directions
+// If no element is found, return -1.
+
+const findIndexOf = (string, substring) => {
+  return
+}
+
+console.log(findIndexOf('hello world', 'world'))
+console.log(findIndexOf('testing', 'ing'))
+console.log(findIndexOf('abcdef', 'xyz'))
