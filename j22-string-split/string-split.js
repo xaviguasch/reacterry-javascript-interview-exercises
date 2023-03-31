98b8adfe36eddf7d380a425e10ca2ef086a6b5d5
Xavi Guasch <xaviguasch@gmail.com>
2023-03-31 19:05:29 +0200

    added j22-string-split EASY challenge

---
+++ j22-string-split/string-split.js
@@ -0,0 +1,14 @@
+// String.split
+// In this coding challenge, you will be implementing a function called splitSentence(sentence) that takes one argument: a sentence. The function should return an array where each element of the array is a word.
+
+// Directions
+// Assume that the input sentence is a valid sentence with each word separated by one whitespace.
+// If there are special characters like ! or ., then treat them like words, unless they are adjacent to an existing word like hurray!, in that case, assume that ! is part of the word.
+
+const splitSentence = (sentence) => {
+  return
+}
+
+console.log(splitSentence('The quick brown fox'))
+console.log(splitSentence('Hello, world!'))
+console.log(splitSentence('OneWord'))
