8b15e7bbd4903e3e573ad67ce7f33eddde14b022
Xavi Guasch <xaviguasch@gmail.com>
2023-03-31 19:18:01 +0200

    added j24-array-unshift EASY challenge

---
+++ j24-array-unshift--EASY/array-unshift.js
@@ -0,0 +1,10 @@
+// Array.unshift
+// In this coding challenge, you will be implementing a function called addFromBeginning(array, value) that takes two arguments: an array and a value. The function should add value to the array from the beginning.
+
+const addFromBeginning = (array, value) => {
+  // Write your code here
+  return
+}
+
+console.log(addFromBeginning([1, 2, 3], 0))
+console.log(addFromBeginning(['foo', 'bar'], 'baz'))
