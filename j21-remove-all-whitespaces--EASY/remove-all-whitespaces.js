d701b4ddd498df4a8d2be59d40fbcac67b2fd88d
Xavi Guasch <xaviguasch@gmail.com>
2023-03-31 18:55:03 +0200

    added j21-remove-all-whitespaces EASY challenge

---
+++ j21-remove-all-whitespaces--EASY/remove-all-whitespaces.js
@@ -0,0 +1,14 @@
+// Remove all whitespaces
+// In this coding challenge, you will be implementing a function called removeWhitespaces(string) that takes a string as the input. The function should return the string with all the whitespace removed.
+
+// Directions
+// If the input string is empty, then return an input string.
+// Remember to account for \n and \t.
+
+const removeWhitespaces = (string) => {
+  return
+}
+
+console.log('Hello,   World!')
+console.log('  This is a sentence.\nIt contains some whitespace.  ')
+console.log('\t  \n\n')
