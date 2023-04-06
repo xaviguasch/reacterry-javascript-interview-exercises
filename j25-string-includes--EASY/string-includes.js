02dd96cc16ba1555932eebdfcb39f3559316e7bf
Xavi Guasch <xaviguasch@gmail.com>
2023-04-06 19:45:10 +0200

    added j26-string-includes EASY challenge

---
+++ j25-string-includes--EASY/string-includes.js
@@ -0,0 +1,9 @@
+// String.includes
+// In this coding challenge, you will be implementing a function called containsSubstring(string, substring) that takes two arguments: a string and a substring. The function should return true if the string contains the substring and false otherwise.
+
+const containsSubstring = (string, substring) => {
+  // Write your code here
+}
+
+console.log(containsSubstring('Hello, world!', 'world'))
+console.log(containsSubstring('The quick brown fox', 'jumped'))
