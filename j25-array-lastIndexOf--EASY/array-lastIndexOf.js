0064badd7388abf31ad712b85062536972cb9c8d
Xavi Guasch <xaviguasch@gmail.com>
2023-04-06 19:41:12 +0200

    added j25-array-lastIndexOf EASY challenge

---
+++ j25-array-lastIndexOf--EASY/array-lastIndexOf.js
@@ -0,0 +1,14 @@
+// Array.lastIndexOf
+// In this coding challenge, you will be implementing a function called findLastIndexOf(array, value) that takes two arguments: an array and a value. The function should return the index of the last occurrence of the value in the array.
+
+// Directions
+// If no element is found, return -1.
+
+const findLastIndexOf = (array, value) => {
+  // Write your code here
+
+  return
+}
+
+console.log(findIndexOf([1, 2, 3, 2], 2))
+console.log(findIndexOf(['foo', 'bar', 'baz', 'bar'], 'baz'))
