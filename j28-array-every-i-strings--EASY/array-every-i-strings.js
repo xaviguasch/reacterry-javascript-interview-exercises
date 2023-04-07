6555c1d3cbc212a5704789166de0097c96238463
Xavi Guasch <xaviguasch@gmail.com>
2023-04-07 10:36:05 +0200

    added j28-array-every-i-strings EASY challenge

---
+++ j28-array-every-i-strings--EASY/array-every-i-strings.js
@@ -0,0 +1,12 @@
+// Array.every I - strings
+// In this coding challenge, you will be implementing a function called validateStrings(array) that takes one argument: an array. The function should check if every element of the array is a string.
+
+// Directions
+// If the input array is empty, return false.
+
+const validateStrings = (array) => {
+  // Write your code here
+}
+
+console.log(validateStrings(['hello', 'world']))
+console.log(validateStrings(['hello', 123]))
