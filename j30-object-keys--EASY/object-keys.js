6253debd01a30f0872c40413dcd1e03905f6d35b
Xavi Guasch <xaviguasch@gmail.com>
2023-04-09 20:58:48 +0200

    added j30-object-keys EASY challenge

---
+++ j30-object-keys--EASY/object-keys.js
@@ -0,0 +1,17 @@
+// Object.keys
+// In this coding challenge, you will be implementing a function called getKeys(object) that takes an argument: an object. The function should return an array will all of the object’s keys.
+
+// Directions
+// If the object has no keys, return an empty array.
+
+const getKeys = (object) => {
+  return
+}
+
+const object = { foo: 1, bar: 2, baz: 3 }
+console.log(getKeys(object)) // Output: ['foo', 'bar', 'baz']
+const object2 = { foo: 1, bar: 2 }
+console.log(getKeys(object2)) // Output: ['foo', 'bar']
+
+const object3 = {}
+console.log(getKeys(object3)) // Output: []
