f53a8409d6a6f0d507f108e4f8c284a452c86fe6
Xavi Guasch <xaviguasch@gmail.com>
2023-04-09 20:51:28 +0200

    added j29-remove-vowels EASY challenge

---
+++ j29-remove-vowels--EASY/remove-vowels.js
@@ -0,0 +1,13 @@
+// Remove vowels
+// Write a function removeVowels(n), which takes a string str as an input. This function should return a new string with all of the vowels removed.
+
+// Directions
+// The function should remove vowels in both lowercase and UPPERCASE.
+
+const removeVowels = (str) => {
+  return
+}
+
+console.log(removeVowels('Hello, World!'))
+console.log(removeVowels('hello world'))
+console.log(removeVowels('programming is fun'))
