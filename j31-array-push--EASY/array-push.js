fbbe556e414f6af5ce82494a75ac0c857b575398
Xavi Guasch <xaviguasch@gmail.com>
2023-04-10 20:41:59 +0200

    added j31-array-push EASY challenge

---
+++ j31-array-push--EASY/array-push.js
@@ -0,0 +1,9 @@
+// Array.push
+// In this coding challenge, you will be implementing a function called append(array, value) that takes two arguments: an array and a value. The function should append the value to the end of the array.
+
+const append = (array, value) => {
+  // Write your code here
+}
+
+console.log(append([1, 2, 3], 4))
+console.log(append(['foo', 'bar'], 'baz'))
