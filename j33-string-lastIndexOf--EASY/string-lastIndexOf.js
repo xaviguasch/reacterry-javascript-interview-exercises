bce02cd149e1ec442fd28ece5e29d1ed7fec6cb0
Xavi Guasch <xaviguasch@gmail.com>
2023-04-10 20:57:34 +0200

    added j33-string-lastIndexOf EASY challenge

---
+++ j33-string-lastIndexOf--EASY/string-lastIndexOf.js
@@ -0,0 +1,13 @@
+// String.lastIndexOf
+// In this coding challenge, you will be implementing a function called findLastIndexOf(string, substring) that takes two arguments: a string and a substring. The function should return the index of the last occurrence of the value in the string.
+
+// Directions
+// If no element is found, return -1.
+
+const findLastIndexOf = (string, substring) => {
+  // Write your code here
+}
+
+console.log(findLastIndexOf('hello world', 'o'))
+console.log(findLastIndexOf('testing', 't'))
+console.log(findLastIndexOf('abcdef', 'xyz'))
