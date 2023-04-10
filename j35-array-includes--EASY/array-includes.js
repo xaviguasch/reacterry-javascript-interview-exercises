40e7550fb6aa29b69a85fe1c16b220eabb11e742
Xavi Guasch <xaviguasch@gmail.com>
2023-04-10 21:10:19 +0200

    added j35-array-includes EASY challenge

---
+++ j35-array-includes--EASY/array-includes.js
@@ -0,0 +1,14 @@
+// Array.includes
+// In this coding challenge, you will be implementing a function called isInArray(array, value) that takes two arguments: an array and a value. The function should return true if the input array includes the value and false otherwise
+
+// Directions
+// If the input array is empty, return false.
+// Assume that the input array will not contain objects.
+
+const isInArray = (array, value) => {
+  // Write your code here
+}
+
+const fruits = ['apple', 'banana', 'orange']
+console.log(isInArray(fruits, 'banana'))
+console.log(isInArray(fruits, 'pear'))
