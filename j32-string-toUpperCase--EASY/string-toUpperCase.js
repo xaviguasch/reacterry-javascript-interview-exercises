2af4b331ecbedb4504bdcf0e9e581951428ec29e
Xavi Guasch <xaviguasch@gmail.com>
2023-04-10 20:51:22 +0200

    added j32-string-toUpperCase EASY challenge

---
+++ j32-string-toUpperCase--EASY/string-toUpperCase.js
@@ -0,0 +1,13 @@
+// String.toUpperCase
+// In this coding challenge, you will be implementing a function called getUpperCase(word) that takes one argument: a word. The function should return the word converted to UPPERCASE.
+
+// Directions
+// If the input word is empty, then return an empty string.
+
+const getUpperCase = (word) => {
+  // Write your code here
+}
+
+console.log(getUpperCase('hello'))
+console.log(getUpperCase('WoRlD'))
+console.log(getUpperCase('tEsT'))
