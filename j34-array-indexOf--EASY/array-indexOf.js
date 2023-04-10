5309b5aef9f27562be2a44c0ba55783e279aa20a
Xavi Guasch <xaviguasch@gmail.com>
2023-04-10 21:06:23 +0200

    added j34-array-indexOf EASY challenge

---
+++ j34-array-indexOf--EASY/array-indexOf.js
@@ -0,0 +1,12 @@
+// Array.indexOf
+// In this coding challenge, you will be implementing a function called findIndexOf(array, value) that takes two arguments: an array and a value. The function should return the index of the first occurrence of the value in the array.
+
+// Directions
+// If no element is found, return -1.
+
+const findIndexOf = (array, value) => {
+  return
+}
+
+console.log(findIndexOf([1, 2, 3], 2))
+console.log(findIndexOf(['foo', 'bar', 'baz'], 'baz'))
