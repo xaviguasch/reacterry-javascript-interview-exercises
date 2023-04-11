c134cf6137f30bf1ec7d967c8a538aae833b7012
Xavi Guasch <xaviguasch@gmail.com>
2023-04-11 21:09:52 +0200

    added j39-string-toLowerCase EASY challenge

---
+++ j39-string-toLowerCase--EASY/string-toLowerCase.js
@@ -0,0 +1,13 @@
+// String.toLowerCase
+// In this coding challenge, you will be implementing a function called getLowerCase(word) that takes one argument: a word. The function should return the word converted to lowercase.
+
+// Directions
+// If the input word is empty, then return an empty string.
+
+const getLowerCase = (word) => {
+  // Write your code here
+}
+
+console.log(getLowerCase('HELLO'))
+console.log(getLowerCase('WoRlD'))
+console.log(getLowerCase('tEsT'))
