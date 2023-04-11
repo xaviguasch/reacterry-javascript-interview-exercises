b5214e21034b4bb484d62dfe8493df91c091cacb
Xavi Guasch <xaviguasch@gmail.com>
2023-04-11 21:13:55 +0200

    added j40-array-find-i-string EASY challenge

---
+++ j40-array-find-i-string--EASY/array-find-i-string.js
@@ -0,0 +1,14 @@
+// Array.find I - string
+// In this coding challenge, you will be implementing a function called getLongerThanFour(array) that takes an array of strings array. This function should return the first element that’s longer than 4 characters.
+
+// Directions
+// If no such element is found, return null.
+// Assume that all elements in the array are strings.
+
+const getLongerThanFour = (array) => {
+  return
+}
+
+console.log(getLongerThanFour(['hello', 'world', 'foo'])) // Output: 'hello'
+console.log(getLongerThanFour(['a', 'ab', 'abc', 'abcd'])) // Output: 'abc'
+console.log(getLongerThanFour(['a', 'ab', 'abc', 'abcd'])) // Output: null
