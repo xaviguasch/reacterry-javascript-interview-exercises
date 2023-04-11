39ee100dc9e79dbd7ff64becad1a8fd22397b953
Xavi Guasch <xaviguasch@gmail.com>
2023-04-11 21:06:53 +0200

    added j38-array-shift EASY challenge

---
+++ j38-array-shift--EASY/array-shift.js
@@ -0,0 +1,9 @@
+// Array.shift
+// In this coding challenge, you will be implementing a function called removeFromBeginning(array) that takes one argument: an array. The function should remove one element from the beginning of the array.
+
+const removeFromBeginning = (array) => {
+  // Write your code here
+}
+
+console.log(removeFromBeginning([1, 2, 3]))
+console.log(removeFromBeginning(['foo', 'bar', 'baz']))
