9765e81b8682575dedca1536f62b1579809478e6
Xavi Guasch <xaviguasch@gmail.com>
2023-04-13 22:28:46 +0200

    added j45-random EASY challenge

---
+++ j45-random--EASY/random.js
@@ -0,0 +1,12 @@
+// Random
+// Write a function random() that takes no arguments and returns a random integer between 0 and Number.MAX_SAFE_INTEGER.
+
+// Directions
+// Should always return an integer between 0 and Number.MAX_SAFE_INTEGER.
+
+const random = () => {
+  // Write your code here
+}
+
+const randomNum = random()
+console.log(randomNum) // e.g. 1234567890
