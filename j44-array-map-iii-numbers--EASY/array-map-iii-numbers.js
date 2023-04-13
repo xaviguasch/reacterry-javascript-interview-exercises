30634e7a52cc8d42071ff5c335b6e3c9b43561da
Xavi Guasch <xaviguasch@gmail.com>
2023-04-13 22:24:43 +0200

    added j44-array-map-iii-numbers EASY challenge

---
+++ j44-array-map-iii-numbers--EASY/array-map-iii-numbers.js
@@ -0,0 +1,16 @@
+// Array.map III - numbers
+// Write a function called doubleNumbers(numbers) that takes an array of numbers as an argument and returns a new array with every element of the input array doubled.
+
+// Directions
+// You should return a new array.
+// If the input array is empty, then return an empty array.
+// The input array will contain only numbers.
+
+const doubleNumbers = (numbers) => {
+  // Edit this line
+}
+
+const numbers = [1, 2, 3, 4, 5]
+const doubledNumbers = doubleNumbers(numbers)
+
+console.log(doubledNumbers) // Output: [2, 4, 6, 8, 10]
