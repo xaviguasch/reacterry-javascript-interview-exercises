754d2e6e4864272a35785b2b5607a11d4a62900d
Xavi Guasch <xaviguasch@gmail.com>
2023-04-13 22:21:48 +0200

    added j43-reverse-words-in-a-string EASY challenge

---
+++ j43-reverse-words-in-a-string--EASY/reverse-words-in-a-string.js
@@ -0,0 +1,12 @@
+// Reverse words in a string
+// Write a function reverseSentence(sentence) that takes a string sentence and returns the sentence with words all reversed.
+
+// Directions
+// You can assume that the input sentence will always be a sequence of words separated by one whitespace.
+
+const reverseSentence = (sentence) => {
+  // Write your code here
+}
+
+console.log(reverseSentence('hello world')) // Output: 'world hello'
+console.log(reverseSentence('reacterry')) // Output: 'reacterry'
