55eef7146ea27283f9c82be37890067535d3c7ea
Xavi Guasch <xaviguasch@gmail.com>
2023-04-14 20:44:46 +0200

    added j46-leap-year EASY challenge

---
+++ j46-leap-year--EASY/leap-year.js
@@ -0,0 +1,14 @@
+// Leap year
+// Write a function isLeapYear(year) that takes in an integer number year as the argument and returns true if year is a leap year, and false otherwise.
+
+// Directions
+// Should return false if year is less than or equal to 0.
+// Should return true if year is a leap year, i.e. it is divisible by 4 or by 400.
+// Should return false if year is not a leap year.
+
+const isLeapYear = (year) => {
+  // Write your code here
+}
+
+console.log(isLeapYear(2000)) // true
+console.log(isLeapYear(2001)) // false
