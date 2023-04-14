b46d16aa11fd5013372c802978d6accca64f0ea1
Xavi Guasch <xaviguasch@gmail.com>
2023-04-14 20:53:25 +0200

    added j47-random-in-range EASY challenge

---
+++ j47-random-in-range--EASY/random-in-range.js
@@ -0,0 +1,13 @@
+// Random in range
+// In this coding challenge, you will be implementing a function called randomInRange(min, max) that generates a random integer between a given minimum value (inclusive) and a maximum value (exclusive). The function should take two arguments, min and max, both of which are integers.
+
+// Directions
+// The minimum value is inclusive and the maximum value is exclusive.
+// Assume that this statement is always true min < max.
+
+const randomInRange = (min, max) => {
+  // Write your code here
+}
+
+const randomInt = randomInRange(1, 10)
+console.log(randomInt) // output: a random integer between 1 and 9
