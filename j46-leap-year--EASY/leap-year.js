201a9aa5f076ed52ef68b2c2f295bfc9fa690aa1
Xavi Guasch <xaviguasch@gmail.com>
2023-04-14 20:50:03 +0200

    added solution to previous challenge

--- j46-leap-year--EASY/leap-year.js
+++ j46-leap-year--EASY/leap-year.js
@@ -8,6 +8,15 @@
 
 const isLeapYear = (year) => {
   // Write your code here
+  if (year <= 0) {
+    return false
+  }
+
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 console.log(isLeapYear(2000)) // true
