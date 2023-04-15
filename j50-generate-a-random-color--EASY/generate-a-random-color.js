c3f547f2b267ef0a63922cac6e1cc20d1509e9fd
Xavi Guasch <xaviguasch@gmail.com>
2023-04-15 15:54:43 +0200

    added j50-generate-a-random-color EASY challenge

---
+++ j50-generate-a-random-color--EASY/generate-a-random-color.js
@@ -0,0 +1,13 @@
+// Generate a random color
+// Write a function getRandomColor(), which takes in no arguments. The function has to return a random color in a hexadecimal format.
+
+// Hex color codes start with a pound sign or hashtag (#) and are followed by six characters.
+
+// Directions
+// A valid hex color code can only contain the digits 0-9 and the letters A-F or a-f, in any combination.
+
+const getRandomColor = () => {
+  // Write your code here
+}
+
+console.log(getRandomColor())
