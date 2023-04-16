a182aab81b1f609024cf3dfca14a9663fc1ada8c
Xavi Guasch <xaviguasch@gmail.com>
2023-04-16 16:59:26 +0200

    added j52-invert-an-object INTER challenge

---
+++ j52-invert-an-object--INTER/invert-an-object.js
@@ -0,0 +1,21 @@
+// Invert an object
+// Write a function invertObject(obj), which takes an object obj as an argument and returns a new object where the keys and values of the original object are swapped.
+
+// Directions
+// The values of the input object can only be strings and numbers
+// If the input object is empty, then return an empty object
+// The function should not modify the original object
+// The function should handle objects with any number of key-value pairs
+
+const invertObject = (obj) => {
+  return
+}
+
+const originalObj = {
+  name: 'John',
+  age: 30,
+  city: 'New York',
+}
+
+const invertedObj = invertObject(originalObj)
+console.log(invertedObj) // Output: { John: 'name', '30': 'age', 'New York': 'city' }
