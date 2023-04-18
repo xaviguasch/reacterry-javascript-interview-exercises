80647dc35d6671adfbe2969170aa0e26cc6e06e8
Xavi Guasch <xaviguasch@gmail.com>
2023-04-18 19:37:39 +0200

    added alternative solution to previous challenge

--- j54-max-character--INTER/max-character.js
+++ j54-max-character--INTER/max-character.js
@@ -39,3 +39,21 @@ console.log(maxCharacters('hello'))
 console.log(maxCharacters('Mississippi'))
 console.log(maxCharacters(''))
 console.log(maxCharacters('a b c d e'))
+
+// ALTERNATIVE SOLUTION
+
+// export const maxCharacters = (str) => {
+//   let charCount = {};
+//   for (let char of str) {
+//     charCount[char] = charCount[char] + 1 || 1;
+//   }
+//   let maxChar = null;
+//   let maxCount = 0;
+//   for (let char in charCount) {
+//     if (charCount[char] > maxCount) {
+//       maxChar = char;
+//       maxCount = charCount[char];
+//     }
+//   }
+//   return maxChar;
+// }
