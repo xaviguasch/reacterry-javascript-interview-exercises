97ca9484ee135d7350bfa00409081ad17151ceef
Xavi Guasch <xaviguasch@gmail.com>
2023-04-18 19:26:39 +0200

    added j54-max-characters INTER challenge

---
+++ j54-max-character--INTER/max-character.js
@@ -0,0 +1,15 @@
+// Max character
+// Write a function maxCharacters(str), which takes a string str as an input. This function should return the character that occurs most often.
+
+// Directions
+// If the input string is empty, return null.
+// If two or more characters appear in the str with the same frequency, return the one whose first occurrence has lower index.
+
+const maxCharacters = (str) => {
+  // Write your code here
+}
+
+console.log(maxCharacters('hello'))
+console.log(maxCharacters('Mississippi'))
+console.log(maxCharacters(''))
+console.log(maxCharacters('a b c d e'))
