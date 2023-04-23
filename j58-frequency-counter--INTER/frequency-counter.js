713a43f5ce2520154038567082ef95fac44f4fc4
Xavi Guasch <xaviguasch@gmail.com>
2023-04-23 22:44:39 +0200

    added j58-frequency-counter INTER challenge

---
+++ j58-frequency-counter--INTER/frequency-counter.js
@@ -0,0 +1,17 @@
+// Frequency counter
+// Implement a function called frequencyCounter(arr) that takes in an array arr of elements and returns an object that counts how many times each element appears in the array.
+
+// The counter object should be structured as a key-value pair, where each key represents an element from the input array and each value represents the number of times that element appears in the input array.
+
+// Directions
+// The input array will contain only strings and numbers.
+// If the input array is empty, return an empty object.
+
+const frequencyCounter = (arr) => {
+  // Write your code here
+}
+
+const arr = [1, 2, 3, 1, 2, 1, 4, 5, 6, 5]
+const counter = frequencyCounter(arr)
+console.log(counter)
+// Output: { '1': 3, '2': 2, '3': 1, '4': 1, '5': 2, '6': 1 }
