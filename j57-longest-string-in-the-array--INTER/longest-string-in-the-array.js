8f96b5e5172a62780f2f950adda77c79fbb226d5
Xavi Guasch <xaviguasch@gmail.com>
2023-04-23 22:32:07 +0200

    added j57-longest-string-in-the-array INTER challenge

---
+++ j57-longest-string-in-the-array--INTER/longest-string-in-the-array.js
@@ -0,0 +1,15 @@
+// Longest string in the array
+// In this coding challenge, you will be implementing a function called findLongest(array) that takes an array of strings array as the argument. The function should return the longest string in the array.
+
+// Directions
+// If the input array is empty, then return null.
+// If two or more strings have the same length, return the one with a lower index.
+
+const findLongest = (array) => {
+  // Write your code here
+}
+
+console.log(findLongest(['cat', 'dog', 'elephant'])) // Output: 'elephant'
+console.log(findLongest(['apple', 'banana', 'pear'])) // Output: 'banana'
+console.log(findLongest(['', 'a', 'aa', 'aaa'])) // Output: 'aaa'
+console.log(findLongest([])) // Output: null
