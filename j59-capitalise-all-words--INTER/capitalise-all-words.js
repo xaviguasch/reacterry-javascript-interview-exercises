ed5f31bdb3495643fb9036892ab46dd55101df0b
Xavi Guasch <xaviguasch@gmail.com>
2023-04-24 09:56:23 +0200

    added j59-capitalise-all-words INTER challenge

---
+++ j59-capitalise-all-words--INTER/capitalise-all-words.js
@@ -0,0 +1,15 @@
+// Capitalise all words
+// In this coding challenge, you will be implementing a custom capitaliseAllWords(input) function. The function should take one string argument called input. The input string can consist of one or more words separated by a white space (just like a regular sentence). The function should capitalise all of the words in the string. Keep in mind that the input string can include numbers and special characters.
+
+// Directions
+// Your function should return a new string.
+// If the input is an empty string, then return an empty string.
+// The input will always be a valid sentence.
+
+const capitaliseAllWords = (input) => {
+  // Write your code here
+}
+
+console.log(capitaliseAllWords('hello world! 123')) // Output: "Hello World! 123"
+console.log(capitaliseAllWords('how are you mate')) // Output: "How Are You Mate"
+console.log(capitaliseAllWords('aNGELA tHE cAT lADY')) // Output: "ANGELA THE CAT LADY"
