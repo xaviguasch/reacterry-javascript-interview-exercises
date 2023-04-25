ca4adbf526d4af96254aae73052b1b4412670f40
Xavi Guasch <xaviguasch@gmail.com>
2023-04-25 16:09:22 +0200

    added j61-sum-numbers-from-a-string INTER challenge

---
+++ j61-sum-numbers-from-a-string--INTER/sum-numbers-from-a-string.js
@@ -0,0 +1,15 @@
+// Sum numbers from a string
+// Write a function sumNumbers(str), which takes a string str as an argument. The string represents a list of numbers separated by a comma. This function should return the sum of all the numbers in the string.
+
+// Directions
+// Assume that the input str will always be a valid sequence of comma-separated integers.
+// If the input string is empty, then return 0.
+
+const sumNumbers = (str) => {
+  // Write your code here
+}
+
+console.log(sumNumbers('')) // Output: 0
+console.log(sumNumbers('1,2,3,4,5')) // Output: 15
+console.log(sumNumbers('0,0,1,2,2')) // Output: 5
+console.log(sumNumbers('10,-20,30,-40,50')) // Output: 30
