6ef1fa4188810e56c129c8fbf8f8a3307760ab08
Xavi Guasch <xaviguasch@gmail.com>
2023-04-28 21:22:06 +0200

    added j62-get-strings-starting-with-a-vowel INTER challenge

---
+++ j62-get-strings-starting-with-a-vowel--INTER/get-strings-starting.js
@@ -0,0 +1,16 @@
+// Get strings starting with a vowel
+// In this coding challenge, you will be implementing a function called getStringsWithVowels(array) that takes an array of strings array as the argument. The function should return a new array with strings starting with vowels.
+
+// Directions
+// If the input array is empty, then return an empty array.
+
+const getStringsWithVowels = (array) => {
+  // Write your code here
+}
+
+console.log(getStringsWithVowels(['apple', 'banana', 'orange', 'pear'])) // Output: ['apple', 'orange']
+
+console.log(getStringsWithVowels(['Elephant', 'Zebra', 'Ostrich', 'Umbrella'])) // Output: ['Elephant', 'Umbrella']
+
+console.log(getStringsWithVowels(['cat', 'dog', 'elephant', 'pig'])) // Output: ['elephant']
+console.log(getStringsWithVowels([])) // Output: []
