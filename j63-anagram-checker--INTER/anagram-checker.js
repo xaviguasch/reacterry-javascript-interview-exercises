b7ba234b70ef96160e56da75eba4df7da195e563
Xavi Guasch <xaviguasch@gmail.com>
2023-04-29 20:31:59 +0200

    added j63-anagram-checker INTER challenge

---
+++ j63-anagram-checker--INTER/anagram-checker.js
@@ -0,0 +1,18 @@
+// Anagram checker
+// Write a function areAnagrams(strOne, strTwo), which takes two strings: strOne and strTwo as arguments. The function should return true if the two strings are anagrams and false otherwise.
+
+// An anagram is a word or phrase formed by rearranging the letters of another word or phrase. In other words, two strings are considered anagrams if they have exactly the same characters but in a different order.
+
+// Directions
+// The function areAnagrams should be case-insensitive
+
+const areAnagrams = (strOne, strTwo) => {
+  // Write your code here
+}
+
+console.log(areAnagrams('cinema', 'iceman'))
+// Output: true
+console.log(areAnagrams('restful', 'fluster'))
+// Output: true
+console.log(areAnagrams('hello', 'world'))
+// Output: false
