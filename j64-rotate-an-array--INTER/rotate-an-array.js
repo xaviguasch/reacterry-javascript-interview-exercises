692325785f5459e53562cab101876484a3568c8b
Xavi Guasch <xaviguasch@gmail.com>
2023-05-01 20:49:28 +0200

    added j64-rotate-an-array INTER exercise

---
+++ j64-rotate-an-array--INTER/rotate-an-array.js
@@ -0,0 +1,16 @@
+// Rotate an array
+// Implement a function called rotateArray(arr, n) that takes an array arr and a number n as arguments. It should return a new array with the elements rotated to the left by n places.
+
+// Directions
+// The input array can contain strings and numbers
+// The value of n can be any positive integer
+// If n is greater than the length of the array, the elements should be rotated more than once.
+
+const rotateArray = (arr, n) => {
+  // Write your code here
+}
+
+console.log(rotateArray([1, 2, 3, 4, 5], 2)) // returns [3, 4, 5, 1, 2]
+console.log(rotateArray(['apple', 'banana', 'cherry', 'date'], 3))
+// ["date", "apple", "banana", "cherry"]
+console.log(rotateArray([1, 2, 3, 4, 5, 6], 4)) // [5, 6, 1, 2, 3, 4]
