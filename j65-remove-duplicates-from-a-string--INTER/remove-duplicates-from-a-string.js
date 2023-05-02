c6505ca702af8861e39e73063868a1bdec81bdb2
Xavi Guasch <xaviguasch@gmail.com>
2023-05-02 09:54:53 +0200

    added j65-remove-duplicates-from-a-string INTER exercise

---
+++ j65-remove-duplicates-from-a-string--INTER/remove-duplicates-from-a-string.js
@@ -0,0 +1,14 @@
+// Remove duplicates from a string
+// Write a function removeDuplicates(str) that takes a string str and returns a new string with all duplicate characters removed.
+
+// Directions
+// If the input string is empty, return an empty string.
+// The function should be case-sensitive.
+// The function should not modify the relative order of the characters in the string.
+
+const removeDuplicates = (str) => {}
+
+console.log(removeDuplicates('hello')) // Output: "helo"
+console.log(removeDuplicates('AAbb')) // Output: "Ab"
+console.log(removeDuplicates('abcdef')) //  Output: "abcdef"
+console.log(removeDuplicates('')) // Output: ""
