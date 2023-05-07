48641cc28079532ba69a66bdc8db667b21a48431
Xavi Guasch <xaviguasch@gmail.com>
2023-05-07 15:41:06 +0200

    added j69-most-common INTER exercise

---
+++ j69-most-common--INTER/most-common.js
@@ -0,0 +1,14 @@
+// Most common
+// In this coding challenge, you will be implementing a function mostCommon(words) that takes an array of strings words as its argument and should return the string that appears the most often. If there are two or more elements that appear the same number of times, then the function should the element whose’ first occurrence has a smaller index.
+
+// Directions
+// If the input array is empty then return null.
+// The input array can only contain strings and numbers.
+
+const mostCommon = (arr) => {
+  // Write your code here
+}
+
+const words = ['apple', 'banana', 'apple', 'cherry', 'banana', 'apple']
+const mostCommonWord = mostCommon(words)
+console.log(mostCommonWord) // "apple"
