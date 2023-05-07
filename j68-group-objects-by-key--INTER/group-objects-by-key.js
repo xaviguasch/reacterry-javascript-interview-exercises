46cf6d058e537ee924ad3740d4d7763696d418d0
Xavi Guasch <xaviguasch@gmail.com>
2023-05-07 12:15:09 +0200

    added j68-group-objects-by-key INTER exercise

---
+++ j68-group-objects-by-key--INTER/group-objects-by-key.js
@@ -0,0 +1,34 @@
+// Group objects by key
+// Write a function groupBy(array, key) that takes an array of objects array and a key as inputs. The function should group the objects by the value of the key and return an object with the groups as keys and the objects as values.
+
+// Directions
+// You can assume that all of the objects have the same properties.
+// You can assume that the key always exists in the objects.
+// If the input array is empty, return an empty object.
+
+const groupBy = (array, key) => {
+  // Write your code here
+}
+
+const people = [
+  { name: 'Alice', age: 28 },
+  { name: 'Bob', age: 35 },
+  { name: 'Charlie', age: 28 },
+  { name: 'David', age: 42 },
+]
+
+const result = groupBy(people, 'age')
+console.log(result)
+
+// {
+//   28: [
+//     { name: 'Alice', age: 28 },
+//     { name: 'Charlie', age: 28 }
+//   ],
+//   35: [
+//     { name: 'Bob', age: 35 }
+//   ],
+//   42: [
+//     { name: 'David', age: 42 }
+//   ]
+// }
