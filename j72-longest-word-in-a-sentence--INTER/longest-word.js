3467335ceba70e54f57a82ecfd36bc4b706c4932
Xavi Guasch <xaviguasch@gmail.com>
2023-05-19 20:22:29 +0200

    added j72-longest-word INTER exercise

---
+++ j72-longest-word-in-a-sentence--INTER/longest-word.js
@@ -0,0 +1,13 @@
+// Longest word in a sentence
+// In this coding challenge, you will be implementing a function findLongestWord(sentence) that takes a string sentence as its argument and returns the longest word in it. If there are two or more words of the same length, then the function should return the word whose’ first occurrence has a smaller index.
+
+// Directions
+// If no such word is found then return null.
+// You can assume that the sentence is always a valid and properly formatted human sentence where words are separated by just one whitespace.
+
+const findLongestWord = (sentence) => {
+  // Write your code here
+}
+
+const sentence = 'Welcome to reacterry'
+console.log(findLongestWord(sentence)) // Output: 'reacterry'
