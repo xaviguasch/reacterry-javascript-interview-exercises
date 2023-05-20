72ef52c1e1f5ab6154a679af28edd27bfed2172a
Xavi Guasch <xaviguasch@gmail.com>
2023-05-20 20:04:11 +0200

    added j73-palindrome-checker INTER exercise

---
+++ j73-palindrome-checker--INTER/palindrome-checker.js
@@ -0,0 +1,20 @@
+// Palindrome checker
+// Write a function isPalindrome(str) that takes a string str and returns true if the string is a palindrome and false otherwise.
+
+// A palindrome is a word, phrase, number, or another sequence of characters that reads the same forward and backwards. For example, "racecar" is a palindrome because it reads the same way forwards and backwards.
+
+// Your implementation should not be case-sensitive, meaning that "Racecar" and "rACeCaR" should both be considered palindromes.
+
+// Directions
+// If the input string is empty then return true.
+// Your implementation should be case-insensitive.
+// The input string will not contain any whitespace.
+// You can assume that the input str will contain only letters.
+
+const isPalindrome = (str) => {
+  // Edit this line
+}
+
+console.log(isPalindrome('racecar')) // Output: true
+console.log(isPalindrome('hello')) // Output: false
+console.log(isPalindrome('Aa')) // Output: true
