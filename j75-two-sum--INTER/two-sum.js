08f52e786d502d196a2111d57348f7466b12be1e
Xavi Guasch <xaviguasch@gmail.com>
2023-05-23 23:07:55 +0200

    added j75-two-sum INTER exercise

---
+++ j75-two-sum--INTER/two-sum.js
@@ -0,0 +1,17 @@
+// Two Sum
+// In this challenge, you need to write a function called twoSum(nums, target), which takes in two arguments: an array of integers nums and a target integer. The function has to find the indices of two numbers such that they add up to the target.
+
+// You can assume that each input would have exactly one solution, integers in nums are unique and you may not use the same element twice.
+
+// Directions
+// You can assume that the input array nums will always have at least 2 integers.
+// You can return the indices in any order.
+
+const twoSum = (nums, target) => {
+  // Write your code here
+}
+
+console.log(twoSum([2, 7, 11, 15], 9))
+// Output: [0,1]
+twoSum([2, 7, 4, 15], 11) // Output: [1,2]
+twoSum([2, 7, 13, 15], 28) // Output: [3,4]
