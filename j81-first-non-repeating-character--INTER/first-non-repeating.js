cbe11c71d54c9de1dfe9e3a7cc580c2489077fa8
Xavi Guasch <xaviguasch@gmail.com>
2023-06-02 20:23:13 +0200

    added j80-first-non-repeating-character INTER exercise

---
+++ j81-first-non-repeating-character--INTER/first-non-repeating.js
@@ -0,0 +1,14 @@
+// First non-repeating character
+// Write a function findCharacter(str), which takes a string str as the argument. The function should return the first non-repeating character.
+
+// Directions
+// If no such character is found, return null.
+// The input string str will not contain whitespace.
+
+const findCharacter = (str) => {
+  // Write your code here
+}
+
+console.log(findCharacter('hello')) // Output: "h"
+console.log(findCharacter('abbcddee')) // Output: "c"
+console.log(findCharacter('aabbcc')) // Output: null
