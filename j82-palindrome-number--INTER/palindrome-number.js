e96d87d498f1c0fa1bf5a82238e46f57f8412eee
Xavi Guasch <xaviguasch@gmail.com>
2023-06-03 19:19:31 +0200

    added j81-palindrome-checker INTER exercise

---
+++ j82-palindrome-number--INTER/palindrome-number.js
@@ -0,0 +1,15 @@
+// Palindrome number
+// Write a function isPalindrome(number) that takes an integer number and returns true if the number is a palindrome and false otherwise.
+
+// A palindrome is a word, phrase, number, or another sequence of characters that reads the same forward and backwards. For example, "racecar" is a palindrome because it reads the same way forwards and backwards.
+
+// Directions
+// You can assume that the input number will always be an integer.
+
+const isPalindrome = (number) => {
+  // Write your code here
+}
+
+console.log(isPalindrome(123321)) // Output: true
+console.log(isPalindrome(12321)) // Output: true
+console.log(isPalindrome(123)) // Output: false
